67c0575a2bc6e5c80622e65b51a344f0acacb19d
Rizkha Tegar <rizkhategar@gmail.com>
2026-06-15 11:06:40 +0700

    Cast Visi Misi rich editor fields

--- app/Models/VisiMisi.php
+++ app/Models/VisiMisi.php
@@ -9,20 +9,27 @@ class VisiMisi extends Model
 {
     use HasFactory;
 
-    // Tambahkan judul_visi dan judul_misi ke sini
     protected $fillable = [
-        'hero_title',          
-        'hero_subtitle',       
+        'hero_title',
+        'hero_subtitle',
         'hero_image',
         'judul_visi',
         'visi',
         'judul_misi',
         'misi',
-        'judul_tujuan',         
-        'tujuan',               
-        'judul_tujuan_bidang',  
-        'tujuan_bidang',        
-        'judul_sasaran_target', 
-        'sasaran_target',       
+        'judul_tujuan',
+        'tujuan',
+        'judul_tujuan_bidang',
+        'tujuan_bidang',
+        'judul_sasaran_target',
+        'sasaran_target',
+    ];
+
+    protected $casts = [
+        'visi' => 'array',
+        'misi' => 'array',
+        'tujuan' => 'array',
+        'tujuan_bidang' => 'array',
+        'sasaran_target' => 'array',
     ];
 }
