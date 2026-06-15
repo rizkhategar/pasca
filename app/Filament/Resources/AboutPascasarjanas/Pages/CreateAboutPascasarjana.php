075281acf762319cde150a361f0ed408fa397028
Rizkha Tegar <rizkhategar@gmail.com>
2026-06-15 14:11:29 +0700

    Map About replacement uploads on create

--- app/Filament/Resources/AboutPascasarjanas/Pages/CreateAboutPascasarjana.php
+++ app/Filament/Resources/AboutPascasarjanas/Pages/CreateAboutPascasarjana.php
@@ -3,9 +3,48 @@
 namespace App\Filament\Resources\AboutPascasarjanas\Pages;
 
 use App\Filament\Resources\AboutPascasarjanas\AboutPascasarjanaResource;
+use App\Models\AboutPascasarjana;
 use Filament\Resources\Pages\CreateRecord;
+use Illuminate\Support\Arr;
 
 class CreateAboutPascasarjana extends CreateRecord
 {
     protected static string $resource = AboutPascasarjanaResource::class;
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        return $this->mapReplacementUploads($data);
+    }
+
+    private function mapReplacementUploads(array $data): array
+    {
+        $data['points'] = collect($data['points'] ?? [])
+            ->map(function (array $point): array {
+                $upload = AboutPascasarjana::normalizeImagePath($point['icon_upload'] ?? null);
+
+                if ($upload) {
+                    $point['icon'] = $upload;
+                } else {
+                    $point['icon'] = AboutPascasarjana::normalizeImagePath($point['icon'] ?? null);
+                }
+
+                Arr::forget($point, 'icon_upload');
+
+                return $point;
+            })
+            ->values()
+            ->all();
+
+        $directorUpload = AboutPascasarjana::normalizeImagePath($data['direktur_image_upload'] ?? null);
+
+        if ($directorUpload) {
+            $data['direktur_image'] = $directorUpload;
+        } else {
+            $data['direktur_image'] = AboutPascasarjana::normalizeImagePath($data['direktur_image'] ?? null);
+        }
+
+        Arr::forget($data, 'direktur_image_upload');
+
+        return $data;
+    }
 }
