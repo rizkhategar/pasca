ab79e33ae2bcb84d1d08bf08a82d5b2618366431
Rizkha Tegar <rizkhategar@gmail.com>
2026-06-15 11:42:05 +0700

    Show latest About Pascasarjana record

--- app/Http/Controllers/AboutController.php
+++ app/Http/Controllers/AboutController.php
@@ -2,18 +2,17 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
-use App\Models\AboutPascasarjana; 
+use App\Models\AboutPascasarjana;
 
 class AboutController extends Controller
 {
     public function index()
     {
-        // Ubah nama variabel menjadi $tentang agar sesuai dengan yang diminta file Blade
-        $tentang = AboutPascasarjana::first();
-        
-        // Kirimkan variabel $tentang ke view 'about'
+        $tentang = AboutPascasarjana::query()
+            ->latest('updated_at')
+            ->latest('id')
+            ->first();
+
         return view('about', compact('tentang'));
     }
 }
