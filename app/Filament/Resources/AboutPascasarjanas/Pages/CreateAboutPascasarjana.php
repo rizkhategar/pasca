b16e9090793beae77199a633779d5f9726a9c7c2
Rizkha Tegar <rizkhategar@gmail.com>
2026-06-17 11:59:13 +0700

    Simplify default About create page after upload handler update

--- app/Filament/Resources/AboutPascasarjanas/Pages/CreateAboutPascasarjana.php
+++ app/Filament/Resources/AboutPascasarjanas/Pages/CreateAboutPascasarjana.php
@@ -8,48 +8,4 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateAboutPascasarjana extends CreateRecord
 {
     protected static string $resource = AboutPascasarjanaResource::class;
-
-    protected function mutateFormDataBeforeCreate(array $data): array
-    {
-        $data['direktur_image'] = $this->extractUploadedPath($data['direktur_image_upload'] ?? null)
-            ?? ($data['direktur_image'] ?? null);
-
-        unset($data['direktur_image_upload']);
-
-        $data['points'] = collect($data['points'] ?? [])
-            ->map(function (array $point): array {
-                $uploadedIcon = $this->extractUploadedPath($point['icon_upload'] ?? null);
-
-                if ($uploadedIcon) {
-                    $point['icon'] = $uploadedIcon;
-                }
-
-                unset($point['icon_upload'], $point['icon_preview']);
-
-                return $point;
-            })
-            ->values()
-            ->all();
-
-        return $data;
-    }
-
-    private function extractUploadedPath(mixed $value): ?string
-    {
-        if (is_string($value) && $value !== '') {
-            return $value;
-        }
-
-        if (is_array($value)) {
-            foreach (array_reverse($value) as $item) {
-                $path = $this->extractUploadedPath($item);
-
-                if ($path) {
-                    return $path;
-                }
-            }
-        }
-
-        return null;
-    }
 }
