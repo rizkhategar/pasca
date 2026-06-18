a5c95c086433ad50fa22540f9e576a2ea08c9f91
Rizkha Tegar <rizkhategar@gmail.com>
2026-06-18 14:13:45 +0700

    Provide configurable WhatsApp admins to Contact page

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -2,12 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Contact;
 use Illuminate\View\View;
 
 class ContactController extends Controller
 {
     public function index(): View
     {
-        return view('contact.index');
+        $contact = Contact::query()->latest('updated_at')->latest('id')->first();
+
+        $whatsappAdmins = [
+            [
+                'name' => $contact?->primary_admin_name ?: 'Admin 1',
+                'number' => $contact?->primary_whatsapp ?: '+62 857-3033-9469',
+                'url' => Contact::whatsappUrl($contact?->primary_whatsapp ?: '+62 857-3033-9469'),
+            ],
+            [
+                'name' => $contact?->secondary_admin_name ?: 'Admin 2',
+                'number' => $contact?->secondary_whatsapp ?: '+62 811-2758-575',
+                'url' => Contact::whatsappUrl($contact?->secondary_whatsapp ?: '+62 811-2758-575'),
+            ],
+        ];
+
+        return view('contact.index', compact('whatsappAdmins'));
     }
 }
