bd3461ccbdf3743f968040b2d848f5deec7d91ff
Rizkha Tegar <rizkhategar@gmail.com>
2026-06-18 14:15:38 +0700

    Render configurable WhatsApp modal on Contact page

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -3,11 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Models\Contact;
-use Illuminate\View\View;
+use Illuminate\Http\Response;
 
 class ContactController extends Controller
 {
-    public function index(): View
+    public function index(): Response
     {
         $contact = Contact::query()->latest('updated_at')->latest('id')->first();
 
@@ -24,6 +24,10 @@ class ContactController extends Controller
             ],
         ];
 
-        return view('contact.index', compact('whatsappAdmins'));
+        $viewData = compact('whatsappAdmins');
+        $page = view('contact.index', $viewData)->render();
+        $modal = view('component.contact-whatsapp-modal', $viewData)->render();
+
+        return response(str_replace('</body>', $modal . '</body>', $page));
     }
 }
