d82d0d2544e14753665d5d7f1f48a951acc76719
Rizkha Tegar <rizkhategar@gmail.com>
2026-06-18 14:56:49 +0700

    Organize academic view under profile directory

---
+++ resources/views/profil/akademik.blade.php
@@ -0,0 +1,6 @@
+{{--
+    Profile view entry point.
+    The current full academic template remains in resources/views/akademik.blade.php
+    during this compatibility transition so no existing layout or asset reference breaks.
+--}}
+@include('akademik')
