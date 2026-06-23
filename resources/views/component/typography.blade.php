26f9e40c14e3075577d5ede0f560dee1aa1d0d47
Rizkha Tegar <rizkhategar@gmail.com>
2026-06-23 08:30:17 +0700

    Load centralized Vite frontend assets

--- resources/views/component/typography.blade.php
+++ resources/views/component/typography.blade.php
@@ -1,1 +1,6 @@
-{{-- Typography CSS has been centralized in resources/css/app.css. --}}
+@once
+    <meta name="pasca-contact-url" content="{{ route('contact.index') }}">
+    <meta name="pasca-about-url" content="{{ route('tentang') }}">
+    <meta name="pasca-vision-mission-url" content="{{ route('visi-misi') }}">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+@endonce
