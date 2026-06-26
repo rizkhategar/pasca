a5af870fc2032b74dd743f9ec1e29810f7e088a0
Rizkha Tegar <rizkhategar@gmail.com>
2026-06-26 08:58:01 +0700

    Add about postgraduates list page

---
+++ app/Filament/Resources/AboutPostgraduates/Pages/ListAboutPostgraduates.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Filament\Resources\AboutPostgraduates\Pages;
+
+use App\Filament\Resources\AboutPostgraduates\AboutPostgraduateResource;
+use App\Models\AboutPostgraduate;
+use Filament\Actions\CreateAction;
+use Filament\Resources\Pages\ListRecords;
+
+class ListAboutPostgraduates extends ListRecords
+{
+    protected static string $resource = AboutPostgraduateResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            CreateAction::make()->hidden(fn () => AboutPostgraduate::count() > 0),
+        ];
+    }
+}
