24e15bbd65cf43906c3b6ab5ac46b0c500b23772
Rizkha Tegar <rizkhategar@gmail.com>
2026-06-26 09:21:39 +0700

    Move typography partial to components folder

---
+++ resources/views/components/typography.blade.php
@@ -0,0 +1,6 @@
+@once
+    <meta name="pasca-contact-url" content="{{ route('contact.index') }}">
+    <meta name="pasca-about-url" content="{{ route('tentang') }}">
+    <meta name="pasca-vision-mission-url" content="{{ route('visi-misi') }}">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+@endonce
