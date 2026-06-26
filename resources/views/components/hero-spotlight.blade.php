c1cb492bac66de08c943799bc5a20d04020df42c
Rizkha Tegar <rizkhategar@gmail.com>
2026-06-26 11:30:53 +0700

    Simplify hero ornament to diagonal fold lines

--- resources/views/components/hero-spotlight.blade.php
+++ resources/views/components/hero-spotlight.blade.php
@@ -1,9 +1,9 @@
-<div class="hero-spotlight pointer-events-none absolute inset-y-0 right-0 z-[2] hidden w-[46%] overflow-hidden lg:block" aria-hidden="true">
-    <div class="absolute -right-20 top-0 h-full w-[88%] skew-x-[-18deg] border-l border-white/12 bg-white/[.055] shadow-[inset_24px_0_70px_rgba(255,255,255,.035)]"></div>
-    <div class="absolute -right-28 top-0 h-full w-[66%] skew-x-[-18deg] border-l border-white/10 bg-white/[.035]"></div>
-    <div class="absolute right-[14%] top-0 h-full w-[2px] skew-x-[-18deg] bg-gradient-to-b from-transparent via-white/24 to-transparent"></div>
-    <div class="absolute right-[33%] top-0 h-full w-[1px] skew-x-[-18deg] bg-gradient-to-b from-transparent via-[#f7b500]/38 to-transparent"></div>
-    <div class="absolute right-[7%] top-[18%] h-16 w-16 skew-x-[-18deg] rounded-[20px] border border-white/12 bg-white/[.045]"></div>
-    <div class="absolute right-[26%] bottom-[20%] h-3 w-36 skew-x-[-18deg] rounded-full bg-gradient-to-r from-transparent via-white/28 to-transparent"></div>
-    <div class="absolute right-[18%] bottom-[28%] h-2 w-24 skew-x-[-18deg] rounded-full bg-[#f7b500]/45 shadow-[0_0_28px_rgba(247,181,0,.22)]"></div>
+<div class="hero-spotlight pointer-events-none absolute inset-y-0 right-0 z-[2] hidden w-[48%] overflow-hidden lg:block" aria-hidden="true">
+    <div class="absolute -right-24 top-0 h-full w-[86%] skew-x-[-20deg] bg-white/[.055] shadow-[inset_28px_0_68px_rgba(255,255,255,.035)]"></div>
+    <div class="absolute -right-16 top-0 h-full w-[58%] skew-x-[-20deg] bg-white/[.035]"></div>
+    <div class="absolute right-[8%] top-0 h-full w-px skew-x-[-20deg] bg-gradient-to-b from-transparent via-white/26 to-transparent"></div>
+    <div class="absolute right-[24%] top-0 h-full w-px skew-x-[-20deg] bg-gradient-to-b from-transparent via-white/18 to-transparent"></div>
+    <div class="absolute right-[37%] top-0 h-full w-[2px] skew-x-[-20deg] bg-gradient-to-b from-transparent via-[#f7b500]/45 to-transparent"></div>
+    <div class="absolute right-[16%] bottom-[24%] h-[3px] w-44 skew-x-[-20deg] rounded-full bg-gradient-to-r from-transparent via-white/34 to-transparent"></div>
+    <div class="absolute right-[27%] bottom-[31%] h-2 w-28 skew-x-[-20deg] rounded-full bg-[#f7b500]/50 shadow-[0_0_24px_rgba(247,181,0,.22)]"></div>
 </div>
