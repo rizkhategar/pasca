aa91b86c87ed26d6cabc426cb3925ba6e70d4bfa
Rizkha Tegar <rizkhategar@gmail.com>
2026-06-26 11:27:12 +0700

    Change hero ornament to diagonal fold lines

--- resources/views/components/hero-spotlight.blade.php
+++ resources/views/components/hero-spotlight.blade.php
@@ -1,20 +1,9 @@
-<div class="hero-spotlight pointer-events-none absolute right-[6%] top-1/2 z-[2] hidden h-[300px] w-[360px] -translate-y-1/2 lg:block" aria-hidden="true">
-    <div class="absolute inset-0 rounded-[42px] border border-white/12 bg-white/[.055] shadow-[0_28px_90px_rgba(2,16,38,.22)] backdrop-blur-[2px]"></div>
-    <div class="absolute -left-6 top-8 h-20 w-20 rounded-[28px] border border-[#f7b500]/30 bg-[#f7b500]/16 shadow-[0_22px_48px_rgba(247,181,0,.16)]"></div>
-    <div class="absolute right-7 top-7 h-20 w-20 rounded-full border border-white/18 bg-white/[.055]"></div>
-    <div class="absolute bottom-7 left-7 h-24 w-24 rounded-full border border-white/14 bg-white/[.04]"></div>
-
-    <div class="absolute left-10 top-12 h-12 w-12 rounded-2xl bg-white/10 ring-1 ring-white/16"></div>
-    <div class="absolute left-[94px] top-[68px] h-[2px] w-40 rounded-full bg-gradient-to-r from-[#f7b500]/70 via-white/35 to-transparent"></div>
-    <div class="absolute right-12 top-[62px] h-4 w-4 rounded-full bg-[#f7b500] shadow-[0_0_26px_rgba(247,181,0,.5)]"></div>
-
-    <div class="absolute left-14 top-[136px] grid grid-cols-6 gap-3 opacity-45">
-        <span class="h-1.5 w-1.5 rounded-full bg-white"></span><span class="h-1.5 w-1.5 rounded-full bg-white"></span><span class="h-1.5 w-1.5 rounded-full bg-white"></span><span class="h-1.5 w-1.5 rounded-full bg-[#f7b500]"></span><span class="h-1.5 w-1.5 rounded-full bg-white"></span><span class="h-1.5 w-1.5 rounded-full bg-white"></span>
-        <span class="h-1.5 w-1.5 rounded-full bg-white"></span><span class="h-1.5 w-1.5 rounded-full bg-[#f7b500]"></span><span class="h-1.5 w-1.5 rounded-full bg-white"></span><span class="h-1.5 w-1.5 rounded-full bg-white"></span><span class="h-1.5 w-1.5 rounded-full bg-white"></span><span class="h-1.5 w-1.5 rounded-full bg-white"></span>
-        <span class="h-1.5 w-1.5 rounded-full bg-white"></span><span class="h-1.5 w-1.5 rounded-full bg-white"></span><span class="h-1.5 w-1.5 rounded-full bg-white"></span><span class="h-1.5 w-1.5 rounded-full bg-white"></span><span class="h-1.5 w-1.5 rounded-full bg-[#f7b500]"></span><span class="h-1.5 w-1.5 rounded-full bg-white"></span>
-    </div>
-
-    <div class="absolute bottom-12 right-10 h-[90px] w-[190px] rounded-[30px] border border-white/14 bg-gradient-to-br from-white/[.12] to-white/[.035] shadow-[0_18px_52px_rgba(2,16,38,.18)]"></div>
-    <div class="absolute bottom-[82px] right-[58px] h-3 w-28 rounded-full bg-white/24"></div>
-    <div class="absolute bottom-[60px] right-[58px] h-2 w-20 rounded-full bg-[#f7b500]/55"></div>
+<div class="hero-spotlight pointer-events-none absolute inset-y-0 right-0 z-[2] hidden w-[46%] overflow-hidden lg:block" aria-hidden="true">
+    <div class="absolute -right-20 top-0 h-full w-[88%] skew-x-[-18deg] border-l border-white/12 bg-white/[.055] shadow-[inset_24px_0_70px_rgba(255,255,255,.035)]"></div>
+    <div class="absolute -right-28 top-0 h-full w-[66%] skew-x-[-18deg] border-l border-white/10 bg-white/[.035]"></div>
+    <div class="absolute right-[14%] top-0 h-full w-[2px] skew-x-[-18deg] bg-gradient-to-b from-transparent via-white/24 to-transparent"></div>
+    <div class="absolute right-[33%] top-0 h-full w-[1px] skew-x-[-18deg] bg-gradient-to-b from-transparent via-[#f7b500]/38 to-transparent"></div>
+    <div class="absolute right-[7%] top-[18%] h-16 w-16 skew-x-[-18deg] rounded-[20px] border border-white/12 bg-white/[.045]"></div>
+    <div class="absolute right-[26%] bottom-[20%] h-3 w-36 skew-x-[-18deg] rounded-full bg-gradient-to-r from-transparent via-white/28 to-transparent"></div>
+    <div class="absolute right-[18%] bottom-[28%] h-2 w-24 skew-x-[-18deg] rounded-full bg-[#f7b500]/45 shadow-[0_0_28px_rgba(247,181,0,.22)]"></div>
 </div>
