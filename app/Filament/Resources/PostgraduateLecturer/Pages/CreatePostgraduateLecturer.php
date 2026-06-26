de0ac3a414b448775929843fdf3cc18d3861f7d4
Dimas Rizky <schooldims@gmail.com>
2026-06-26 11:32:45 +0700

    Add renamed postgraduate lecturer create page

---
+++ app/Filament/Resources/PostgraduateLecturer/Pages/CreatePostgraduateLecturer.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\PostgraduateLecturer\Pages;
+
+use App\Filament\Resources\PostgraduateLecturer\PostgraduateLecturerResource;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreatePostgraduateLecturer extends CreateRecord
+{
+    protected static string $resource = PostgraduateLecturerResource::class;
+}
