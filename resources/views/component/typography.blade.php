160424d68613b5e3b78eba7b48e9c367698d1dcb
Rizkha Tegar <rizkhategar@gmail.com>
2026-06-26 09:26:33 +0700

    Remove old component typography partial

--- resources/views/component/typography.blade.php
+++
@@ -1,6 +0,0 @@
-@once
-    <meta name="pasca-contact-url" content="{{ route('contact.index') }}">
-    <meta name="pasca-about-url" content="{{ route('tentang') }}">
-    <meta name="pasca-vision-mission-url" content="{{ route('visi-misi') }}">
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-@endonce
