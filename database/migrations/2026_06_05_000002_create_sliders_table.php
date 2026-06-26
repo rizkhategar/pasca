a8c96d8050c1c0d7da10fc2e9e491995b6d27379
Rizkha Tegar <rizkhategar@gmail.com>
2026-06-26 13:18:34 +0700

    cleanup

--- database/migrations/2026_06_05_000002_create_sliders_table.php
+++ database/migrations/2026_06_05_000002_create_sliders_table.php
@@ -8,6 +8,10 @@ return new class extends Migration
 {
     public function up(): void
     {
+        if (Schema::hasTable('sliders')) {
+            return;
+        }
+
         Schema::create('sliders', function (Blueprint $table) {
             $table->id();
             $table->string('title')->default('Pascasarjana Universitas Ngudi Waluyo');
