530da30d00e808f40d39aa1c3553ba190925017c
Rizkha Tegar <rizkhategar@gmail.com>
2026-06-26 08:58:42 +0700

    Add postgraduate about create page shell

---
+++ app/Filament/Resources/AboutPostgraduates/Pages/CreatePostgraduateAbout.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\AboutPostgraduates\Pages;
+
+use App\Filament\Resources\AboutPostgraduates\AboutPostgraduateResource;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreatePostgraduateAbout extends CreateRecord
+{
+    protected static string $resource = AboutPostgraduateResource::class;
+}
