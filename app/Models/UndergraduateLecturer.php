8ef78cccaf4d8b9c6b4290d45720d9aad9e85dc0
Dimas Rizky <schooldims@gmail.com>
2026-06-29 09:35:51 +0700

    Use undergraduate lecturer model without detail relation

--- app/Models/UndergraduateLecturer.php
+++ app/Models/UndergraduateLecturer.php
@@ -24,14 +24,64 @@ class UndergraduateLecturer extends Model
         return $this->belongsTo(SintaLecturer::class, 'sinta_id', 'sinta_id');
     }
 
-    public function detail()
-    {
-        return $this->hasOne(UndergraduateLecturerDetail::class);
-    }
-
     public function studyPrograms()
     {
         return $this->belongsToMany(StudyProgram::class, 'undergraduate_lecturer_study_programs')
             ->withTimestamps();
+    }
+
+    public function scopusPublications()
+    {
+        return $this->hasMany(SintaScopusPublication::class, 'sinta_id', 'sinta_id');
+    }
+
+    public function scholarPublications()
+    {
+        return $this->hasMany(SintaScholarPublication::class, 'sinta_id', 'sinta_id');
+    }
+
+    public function garudaPublications()
+    {
+        return $this->hasMany(SintaGarudaPublication::class, 'sinta_id', 'sinta_id');
+    }
+
+    public function books()
+    {
+        return $this->hasMany(SintaBookPublication::class, 'sinta_id', 'sinta_id');
+    }
+
+    public function researches()
+    {
+        return $this->hasMany(SintaResearch::class, 'sinta_id', 'sinta_id');
+    }
+
+    public function services()
+    {
+        return $this->hasMany(SintaService::class, 'sinta_id', 'sinta_id');
+    }
+
+    public function researchYearlies()
+    {
+        return $this->hasMany(SintaResearchYearly::class, 'sinta_id', 'sinta_id');
+    }
+
+    public function serviceYearlies()
+    {
+        return $this->hasMany(SintaServiceYearly::class, 'sinta_id', 'sinta_id');
+    }
+
+    public function scopusYearlyStats()
+    {
+        return $this->hasMany(SintaScopusYearlyStat::class, 'sinta_id', 'sinta_id');
+    }
+
+    public function scholarYearlyStats()
+    {
+        return $this->hasMany(SintaScholarYearlyStat::class, 'sinta_id', 'sinta_id');
+    }
+
+    public function garudaYearlyStats()
+    {
+        return $this->hasMany(SintaGarudaYearlyStat::class, 'sinta_id', 'sinta_id');
     }
 }
