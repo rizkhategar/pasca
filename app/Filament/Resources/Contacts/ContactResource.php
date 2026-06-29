d334d617a1bccbeda9136ffd1c926d5f48e9d635
Rizkha Tegar <rizkhategar@gmail.com>
2026-06-29 13:44:49 +0700

    Rapikan label menu kontak

--- app/Filament/Resources/Contacts/ContactResource.php
+++ app/Filament/Resources/Contacts/ContactResource.php
@@ -18,57 +18,19 @@ use UnitEnum;
 class ContactResource extends Resource
 {
     protected static ?string $model = Contact::class;
-
     protected static ?string $slug = 'contacts';
-
     protected static string|BackedEnum|null $navigationIcon = 'heroicon-o-phone';
-
-    protected static ?string $navigationLabel = 'Contact';
-
+    protected static ?string $navigationLabel = 'Contacts';
     protected static ?string $modelLabel = 'Contact';
-
     protected static ?string $pluralModelLabel = 'Contacts';
-
-    protected static string|UnitEnum|null $navigationGroup = 'Contact';
-
+    protected static string|UnitEnum|null $navigationGroup = 'Contacts';
     protected static ?int $navigationSort = 1;
 
-    public static function form(Schema $schema): Schema
-    {
-        return ContactForm::configure($schema);
-    }
-
-    public static function table(Table $table): Table
-    {
-        return ContactsTable::configure($table);
-    }
-
-    public static function getPages(): array
-    {
-        return [
-            'index' => ListContacts::route('/'),
-            'create' => CreateContact::route('/create'),
-            'edit' => EditContact::route('/{record}/edit'),
-        ];
-    }
-
-    public static function canViewAny(): bool
-    {
-        return auth()->user()?->canManageContact() ?? false;
-    }
-
-    public static function canCreate(): bool
-    {
-        return (auth()->user()?->canManageContact() ?? false) && Contact::query()->doesntExist();
-    }
-
-    public static function canEdit(Model $record): bool
-    {
-        return auth()->user()?->canManageContact() ?? false;
-    }
-
-    public static function canDelete(Model $record): bool
-    {
-        return auth()->user()?->canManageContact() ?? false;
-    }
+    public static function form(Schema $schema): Schema { return ContactForm::configure($schema); }
+    public static function table(Table $table): Table { return ContactsTable::configure($table); }
+    public static function getPages(): array { return ['index' => ListContacts::route('/'), 'create' => CreateContact::route('/create'), 'edit' => EditContact::route('/{record}/edit')]; }
+    public static function canViewAny(): bool { return auth()->user()?->canManageContact() ?? false; }
+    public static function canCreate(): bool { return (auth()->user()?->canManageContact() ?? false) && Contact::query()->doesntExist(); }
+    public static function canEdit(Model $record): bool { return auth()->user()?->canManageContact() ?? false; }
+    public static function canDelete(Model $record): bool { return auth()->user()?->canManageContact() ?? false; }
 }
