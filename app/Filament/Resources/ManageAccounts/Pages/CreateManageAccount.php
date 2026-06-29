1a510235dc02259f761b9d652b06131f252d31d1
Rizkha Tegar <rizkhategar@gmail.com>
2026-06-29 13:46:27 +0700

    Pastikan role tersedia saat tambah user

--- app/Filament/Resources/ManageAccounts/Pages/CreateManageAccount.php
+++ app/Filament/Resources/ManageAccounts/Pages/CreateManageAccount.php
@@ -4,6 +4,7 @@ namespace App\Filament\Resources\ManageAccounts\Pages;
 
 use App\Filament\Resources\ManageAccounts\ManageAccountResource;
 use Filament\Resources\Pages\CreateRecord;
+use Spatie\Permission\Models\Role;
 
 class CreateManageAccount extends CreateRecord
 {
@@ -11,6 +12,7 @@ class CreateManageAccount extends CreateRecord
 
     protected function afterCreate(): void
     {
+        Role::findOrCreate($this->record->role);
         $this->record->syncRoles([$this->record->role]);
     }
 }
