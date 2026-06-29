7b32c6d14d30810841967de57ef3197f289bc8dc
Rizkha Tegar <rizkhategar@gmail.com>
2026-06-29 10:36:29 +0700

    Add default delete action to organizational structure edit page

--- app/Filament/Resources/OrganizationalStructures/Pages/ModifyStructure.php
+++ app/Filament/Resources/OrganizationalStructures/Pages/ModifyStructure.php
@@ -3,9 +3,17 @@
 namespace App\Filament\Resources\OrganizationalStructures\Pages;
 
 use App\Filament\Resources\OrganizationalStructures\OrganizationalStructureResource;
+use Filament\Actions\DeleteAction;
 use Filament\Resources\Pages\EditRecord;
 
 class ModifyStructure extends EditRecord
 {
     protected static string $resource = OrganizationalStructureResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            DeleteAction::make(),
+        ];
+    }
 }
