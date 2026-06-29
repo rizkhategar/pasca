a583731fdfa47255dc07abf09df3a6f43613db64
Rizkha Tegar <rizkhategar@gmail.com>
2026-06-29 11:06:47 +0700

    Add manage account edit page

---
+++ app/Filament/Resources/ManageAccounts/Pages/EditManageAccount.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Filament\Resources\ManageAccounts\Pages;
+
+use App\Filament\Resources\ManageAccounts\ManageAccountResource;
+use App\Models\User;
+use Filament\Actions\DeleteAction;
+use Filament\Resources\Pages\EditRecord;
+
+class EditManageAccount extends EditRecord
+{
+    protected static string $resource = ManageAccountResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            DeleteAction::make()
+                ->visible(fn (User $record): bool => auth()->id() !== $record->id),
+        ];
+    }
+}
