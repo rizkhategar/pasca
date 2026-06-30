ee930d32e8c400d6e53c862a5a6d4f98144bdfdf
Rizkha Tegar <rizkhategar@gmail.com>
2026-06-30 15:13:08 +0700

    refactor(models): support user permission aliases

--- app/Policies/UsersPolicy.php
+++ app/Policies/UsersPolicy.php
@@ -2,8 +2,8 @@
 
 namespace App\Policies;
 
-use Illuminate\Foundation\Auth\User as AuthUser;
 use Illuminate\Auth\Access\HandlesAuthorization;
+use Illuminate\Foundation\Auth\User as AuthUser;
 
 class UsersPolicy
 {
@@ -11,61 +11,66 @@ class UsersPolicy
 
     public function viewAny(AuthUser $authUser): bool
     {
-        return $authUser->can('ViewAny:User');
+        return $this->allows($authUser, 'ViewAny');
     }
 
     public function view(AuthUser $authUser): bool
     {
-        return $authUser->can('View:User');
+        return $this->allows($authUser, 'View');
     }
 
     public function create(AuthUser $authUser): bool
     {
-        return $authUser->can('Create:User');
+        return $this->allows($authUser, 'Create');
     }
 
     public function update(AuthUser $authUser): bool
     {
-        return $authUser->can('Update:User');
+        return $this->allows($authUser, 'Update');
     }
 
     public function delete(AuthUser $authUser): bool
     {
-        return $authUser->can('Delete:User');
+        return $this->allows($authUser, 'Delete');
     }
 
     public function deleteAny(AuthUser $authUser): bool
     {
-        return $authUser->can('DeleteAny:User');
+        return $this->allows($authUser, 'DeleteAny');
     }
 
     public function restore(AuthUser $authUser): bool
     {
-        return $authUser->can('Restore:User');
+        return $this->allows($authUser, 'Restore');
     }
 
     public function forceDelete(AuthUser $authUser): bool
     {
-        return $authUser->can('ForceDelete:User');
+        return $this->allows($authUser, 'ForceDelete');
     }
 
     public function forceDeleteAny(AuthUser $authUser): bool
     {
-        return $authUser->can('ForceDeleteAny:User');
+        return $this->allows($authUser, 'ForceDeleteAny');
     }
 
     public function restoreAny(AuthUser $authUser): bool
     {
-        return $authUser->can('RestoreAny:User');
+        return $this->allows($authUser, 'RestoreAny');
     }
 
     public function replicate(AuthUser $authUser): bool
     {
-        return $authUser->can('Replicate:User');
+        return $this->allows($authUser, 'Replicate');
     }
 
     public function reorder(AuthUser $authUser): bool
     {
-        return $authUser->can('Reorder:User');
+        return $this->allows($authUser, 'Reorder');
+    }
+
+    private function allows(AuthUser $authUser, string $action): bool
+    {
+        return $authUser->can($action . ':User') || $authUser->can($action . ':Users');
     }
 }
