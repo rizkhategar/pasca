95a11ce9b25091abae49b5c0c5de04c8f7c92956
Rizkha Tegar <rizkhategar@gmail.com>
2026-06-30 09:31:51 +0700

    Tambahkan grup System untuk Activity Log

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -81,6 +81,10 @@ class AdminPanelProvider extends PanelProvider
                 NavigationGroup::make()
                     ->label('Authentication')
                     ->collapsed(false),
+
+                NavigationGroup::make()
+                    ->label('System')
+                    ->collapsed(false),
             ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\Filament\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\Filament\Pages')
