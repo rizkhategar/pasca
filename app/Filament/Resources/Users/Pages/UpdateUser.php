c70c8118abab254bc3342664f3823fec19149288
Rizkha Tegar <rizkhategar@gmail.com>
2026-06-30 09:30:06 +0700

    Tambahkan tombol impersonate di halaman edit user

--- app/Filament/Resources/Users/Pages/UpdateUser.php
+++ app/Filament/Resources/Users/Pages/UpdateUser.php
@@ -5,10 +5,20 @@ namespace App\Filament\Resources\Users\Pages;
 use App\Filament\Resources\Users\UserResource;
 use Filament\Resources\Pages\EditRecord;
 use Spatie\Permission\Models\Role;
+use STS\FilamentImpersonate\Actions\Impersonate;
 
 class UpdateUser extends EditRecord
 {
     protected static string $resource = UserResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Impersonate::make()
+                ->record($this->getRecord())
+                ->redirectTo(url('/admin')),
+        ];
+    }
 
     protected function afterSave(): void
     {
