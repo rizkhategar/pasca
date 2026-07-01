f7c6333d0bdc6748867809b3ac173ae9842efcf3
Rizkha Tegar <rizkhategar@gmail.com>
2026-07-01 10:16:55 +0700

    fix(home): load section overrides after app styles

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,7 +15,8 @@
     <meta name="pasca-contact-url" content="{{ route('contact.index') }}">
     <meta name="pasca-about-url" content="{{ route('tentang') }}">
     <meta name="pasca-vision-mission-url" content="{{ route('visi-misi') }}">
-    @vite(['resources/css/app.css', 'resources/css/home-info-section.css', 'resources/js/app.js'])
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @vite('resources/css/home-info-section.css')
     @stack('styles')
 </head>
 <body class="@yield('body_class')">
