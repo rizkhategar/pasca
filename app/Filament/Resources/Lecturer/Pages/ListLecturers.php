df6942d04ba1f399c590780171ec8ea1e6d067fa
Dimas Rizky <schooldims@gmail.com>
2026-07-20 11:32:26 +0700

    refactor(lecturers): remove import action from list page

--- app/Filament/Resources/Lecturer/Pages/ListLecturers.php
+++ app/Filament/Resources/Lecturer/Pages/ListLecturers.php
@@ -3,21 +3,9 @@
 namespace App\Filament\Resources\Lecturer\Pages;
 
 use App\Filament\Resources\Lecturer\LecturerResource;
-use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
 
 class ListLecturers extends ListRecords
 {
     protected static string $resource = LecturerResource::class;
-
-    protected function getHeaderActions(): array
-    {
-        return [
-            Actions\Action::make('openImportPage')
-                ->label('Import Lecturers')
-                ->icon('heroicon-o-arrow-down-tray')
-                ->color('warning')
-                ->url(LecturerResource::getUrl('import')),
-        ];
-    }
 }
